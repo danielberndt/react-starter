686b9f4d16ed45daee1b51faab7064428e92eb90
Daniel Berndt <daniel.berndt@gmail.com>
2015-06-18 20:03:49 +0200

    favour `::this.handleEvent` syntax over `autoBind`-decorator (fix #2)

--- src/my-component.js
+++ src/my-component.js
@@ -1,9 +1,4 @@
 import React from "react";
-// Wait until https://github.com/babel/babel-eslint/issues/72 is fixed...
-/*eslint-disable no-unused-vars*/
-import autobind from "autobind-decorator";
-/*eslint-enable no-unused-vars*/
-
 
 export default class MyComponent extends React.Component {
   static displayName = "MyComponent"
@@ -11,7 +6,6 @@ export default class MyComponent extends React.Component {
 
   state = {count: this.props.initialCount}
 
-  @autobind
   handleClick() {
     this.setState({count: this.state.count + 1});
   }
@@ -20,7 +14,7 @@ export default class MyComponent extends React.Component {
     return (
       <div>
         <div>Clicks: {this.state.count}</div>
-        <button onClick={this.handleClick}>Add 1</button>
+        <button onClick={::this.handleClick}>Add 1</button>
       </div>
     );
   }
